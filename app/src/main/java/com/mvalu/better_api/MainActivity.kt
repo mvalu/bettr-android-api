846d0f184bd0a07be7053fc98c6aeb7fa363f08a
Dhaval Rupapara <dhaval.rupapara14@gmail.com>
2020-05-08 19:31:58 +0530

    Added a new app module for fresh start

---
+++ app/src/main/java/com/mvalu/better_api/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mvalu.better_api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
